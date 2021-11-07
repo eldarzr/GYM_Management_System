92126a60d6c5ef5d6fb62f7db15f3c7757c603a3
eldar <eldarzr@post.bgu.ac.il>
2021-11-07 19:59:13 +0200

    add

---
+++ cmake-build-debug/CMakeFiles/SPL_HW1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPL_HW1.dir/main.cpp.o"
+  "SPL_HW1"
+  "SPL_HW1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPL_HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
